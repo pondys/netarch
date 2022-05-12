bf2c830d5a1c05dccb66fc8b4f5843658cae466b
pondys <juventusys@163.com>
2022-05-13 00:15:09 +0800

    netarch main

---
+++ arp.h
@@ -0,0 +1,72 @@
+
+#ifndef __NG_ARP_H__
+#define __NG_ARP_H__
+
+#include <rte_ether.h>
+
+#define ARP_ENTRY_STATUS_DYNAMIC 0
+#define ARP_ENTRY_STATUS_STATIC 1
+
+//Í·²åÈë
+#define LL_ADD(item, list) do{  			\
+	item->prev = NULL;						\
+    item->next = list;						\
+    if (NULL != list) list->prev = item;	\
+	list = item;							\
+} while(0)
+
+#define LL_REMOVE(item, list) do{  							\
+	if (NULL != item->prev) item->prev->next = item->next;	\
+	if (NULL != item->next) item->next->prev = item->prev;	\
+	if (list == item) list = item->next;\
+	item->prev = item->next = NULL;\
+} while(0)
+
+struct arp_entry {
+	uint32_t ip;
+	uint8_t hwaddr[RTE_ETHER_ADDR_LEN];
+	uint8_t status;
+
+	//¶ÔÆë
+	struct arp_entry *next;
+	struct arp_entry *prev;
+};
+
+struct arp_table {
+	struct arp_entry *entries;
+	int count;
+};
+
+static struct arp_table *arpt = NULL;
+
+static struct arp_table *arp_table_instance(void)
+{
+	if (NULL == arpt)
+	{
+	    arpt = rte_malloc("arp table", sizeof(struct arp_table), 0);
+		if (NULL == arpt)
+		{
+		    rte_exit(EXIT_FAILURE, "rte_malloc arp table failed.\n");
+		}
+		memset(arpt, 0, sizeof(struct arp_table));
+	}
+	return arpt;
+}
+
+static uint8_t* ng_get_dst_macaddr(uint32_t dip)
+{
+    struct arp_entry *iter;
+	struct arp_table *table = arp_table_instance();
+
+	for (iter = table->entries; iter != NULL; iter = iter->next)
+	{
+	    if (dip == iter->ip)
+	    {
+	        return iter->hwaddr;
+	    }
+	}
+
+	return NULL;
+}
+
+#endif
